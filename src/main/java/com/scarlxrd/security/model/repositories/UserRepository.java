43e90f9e64aaed89545e18c50657ccc3e5867b8c
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-09-06 21:08:22 -0300

    feat: creation of user repository.

---
+++ src/main/java/com/scarlxrd/security/model/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.scarlxrd.security.model.repositories;
+
+import com.scarlxrd.security.model.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    UserDetails findByLogin(String login);
+}
