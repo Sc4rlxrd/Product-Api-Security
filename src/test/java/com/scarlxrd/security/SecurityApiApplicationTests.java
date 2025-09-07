268a94f07cf2433c89e3e3165dcd0fe8d1654c84
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-09-06 21:10:04 -0300

    feat: creating the database with data persisting with docker volumes.

---
+++ src/test/java/com/scarlxrd/security/SecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarlxrd.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
