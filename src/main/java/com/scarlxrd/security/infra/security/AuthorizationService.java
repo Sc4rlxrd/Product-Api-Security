ee8834bb7d9afa42c8409eefd70ecf3250dfb5d8
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-09-06 21:23:44 -0300

    feat: More security config.

---
+++ src/main/java/com/scarlxrd/security/infra/security/AuthorizationService.java
@@ -0,0 +1,28 @@
+package com.scarlxrd.security.infra.security;
+
+
+import com.scarlxrd.security.model.repositories.UserRepository;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+
+
+}
